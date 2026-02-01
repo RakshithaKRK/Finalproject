6849f3306f33a063aa856109ca1d01fb56487956
RakshithaKRK <Rakshithakr0804@gmail.com>
2026-02-01 16:05:45 +0530

    jai

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,4 +1,7 @@
 package testcases;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import baseclass.Baseclass;
@@ -12,18 +15,23 @@ public class LoginTest extends Baseclass{
 			LP.EnterUsername("practice");
 			LP.EnterPassword("SuperSecretPassword!");
 			LP.ClickLogin();
+			System.out.println("test1");
 		}
 		@Test(priority=1)
 		public void TCC02_LoginTest() {
 			loginpage LP = new loginpage(driver);
 			LP.EnterUsername("practice");
-			LP.EnterPassword("SuperSecretPassword");
+		LP.EnterPassword("SuperSecretPassword");
 			LP.ClickLogin();
+			System.out.println("test2");
+			WebElement e = driver.findElement(By.xpath("//div[@id='flash'])"));
+		String title=e.getText();
+			Assert.assertEquals(title, "Your password is invalid");
 		}
 		@Test(priority=2)
 		public void TCC03_LoginTest() {
 		
-			System.out.println("test");System.out.println("test");
+			System.out.println("test3");
 		}
 	}
 
